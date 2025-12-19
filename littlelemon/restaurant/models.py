cb8b9314c6e319758025610b13e700d09d22f64b
Carlos Lopez <carloslopezr29@gmail.com>
2025-12-19 13:03:04 -0500

    MYSQL setted up

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=100)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.Name} - {self.BookingDate}"
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=100)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Inventory = models.IntegerField()
